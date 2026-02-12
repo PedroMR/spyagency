c63a974397e485e0e51fb4328c4aeb63033d6ce5
Pedro Miller Rabinovitch <pedro@robotogames.com>
2026-02-12 09:18:28 -0500

    New cards and favicon

--- php/index.php
+++ php/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Spy Agency - Lobby</title>
+    <!--link rel="icon" type="image/x-icon" href="img/favicon.ico"-->
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🕵️</text></svg>">
     <link rel="stylesheet" href="css/style.css?v=<?= filemtime(__DIR__.'/css/style.css') ?>">
 </head>
 <body>
