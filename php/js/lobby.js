a535de525ad38a5bcd33db88314fce3187891772
Pedro Miller Rabinovitch <pedro@robotogames.com>
2026-03-03 09:56:07 -0500

    Auto room names

--- php/js/lobby.js
+++ php/js/lobby.js
@@ -171,9 +171,105 @@ async function apiGet(url) {
     return json;
 }
 
+const ROOM_NAME_WORDS = [
+    'ace','axe','bay','bee','box','cap','cod','cup','dew','eel','elm','fan',
+    'fox','gem','gnu','hat','hen','ivy','jar','jay','jug','koi','lid','log',
+    'map','mug','oak','owl','pen','pig','pit','pod','ram','rat','rod','rug',
+    'rye','saw','sky','sun','tin','urn','van','yak',
+    'arch','bear','bell','bird','boar','bolt','book','boot','bowl','buck',
+    'bull','cage','cake','calf','carp','cart','cave','clam','club','coat',
+    'colt','crab','crop','crow','cube','dart','dawn','deck','deer','dice',
+    'dome','dove','drum','duck','dune','dust','fawn','fish','flag','foam',
+    'fork','fort','frog','gate','gnat','goat','gold','gust','halo','harp',
+    'hawk','herb','hill','hook','horn','ibis','isle','jade','kite','knot',
+    'lace','lake','lamp','land','lark','lava','lawn','leaf','lens','lime',
+    'lion','loft','lynx','mace','mast','maze','mesh','mint','mist','mole',
+    'moon','moor','moss','moth','mule','nail','nest','newt','node','nook',
+    'opal','orca','pace','pack','pane','park','path','peak','pier','pike',
+    'pine','plum','pond','pool','port','post','puma','pump','rack','ramp',
+    'reef','rice','ring','rock','roof','rope','rose','ruin','rush','rust',
+    'sage','sail','salt','sand','seed','shed','ship','silk','slab','slug',
+    'snap','snow','soap','soil','song','soot','spar','spot','star','stem',
+    'stew','surf','swan','tank','teak','tent','tide','tile','toad','toll',
+    'tomb','tome','tram','trap','tray','tube','tusk','vale','vane','vase',
+    'veil','vein','vent','vine','void','volt','vole','wake','wall','wand',
+    'wasp','wolf','wood','wool','wren','yard','yarn','yoke','zinc','zone',
+    'abbey','acorn','amber','anvil','arbor','arena','armor','aroma','attic',
+    'azure','badge','basil','basin','bench','berry','birch','blade','blaze',
+    'bloom','board','braid','bream','briar','brine','brook','broom','broth',
+    'brush','butte','cabin','cairn','canal','cargo','cedar','chalk','charm',
+    'chest','chime','chord','cider','cinch','claim','clamp','cleft','cliff',
+    'cloak','cloth','cloud','clove','coral','couch','craft','crane','crest',
+    'crisp','croak','crook','crowd','crush','curly','curve','cycle','delta',
+    'depot','eagle','easel','elbow','elder','emery','fable','feast','fetch',
+    'fiber','finch','flair','flake','flame','flask','flint','flock','flood',
+    'flora','flute','forge','forum','frail','frond','frost','fungi','gauze',
+    'gavel','gecko','ghost','girth','glare','glass','gleam','glide','globe',
+    'gloom','glory','gloss','glove','gnome','goose','gourd','grace','grain',
+    'grove','guile','guise','gulch','gusto','halve','haven','hazel','heist',
+    'heron','holly','horse','horde','humus','husky','hydra','idiom','igloo',
+    'image','ingot','inlet','ivory','jaunt','jelly','joust','kayak','knave',
+    'knoll','larch','latch','lemon','liner','lodge','lunar','maize','manor',
+    'maple','marsh','maxim','mayor','moose','motif','mound','mount','mouse',
+    'mural','myrrh','naive','nerve','noble','notch','nymph','oaken','optic',
+    'orbit','otter','panda','pasta','patch','peach','perch','phase','pivot',
+    'place','plank','plaza','plume','pouch','prawn','prism','probe','prowl',
+    'quail','quartz','queen','quest','quota','raven','realm','resin','ridge',
+    'rivet','robin','roost','rouge','rowdy','ruddy','rusty','sabre','scone',
+    'scorn','scowl','scout','scythe','shard','shark','sheep','shelf','shell',
+    'shine','shire','shrub','siege','sigma','skiff','skimp','skunk','slate',
+    'sleek','sleet','sloth','snail','snake','snipe','spire','spool','spore',
+    'spout','spray','sprig','spunk','squid','stack','stag','stalk','steed',
+    'steel','steep','stern','stoat','stone','stork','storm','stout','straw',
+    'stray','strip','strut','stump','surge','swamp','swarm','swath','swirl',
+    'talon','tapir','thorn','thyme','tiger','torch','totem','trout','truce',
+    'truss','tulip','tuner','tunic','tuple','tweed','twill','twine','twist',
+    'umbra','uncle','unity','untie','unwed','usher','viper','visor','vixen',
+    'vortex','wader','waltz','wafer','waist','walrus','whelk','whirl','whisk',
+    'wight','witch','wraith','wreck','wrist','yodel','zebra','zilch','zippy',
+    'badger','beacon','beaver','belfry','boggle','bonnet','bonfire','breach',
+    'bridle','buster','canary','canopy','castle','cavern','chorus','cipher',
+    'citrus','cobalt','cobweb','coffin','coffer','column','condor','copper',
+    'cornet','corsair','cougar','coyote','crater','crevice','crimson','cuckoo',
+    'dagger','damsel','donkey','dragon','draper','falcon','fallow','famine',
+    'fathom','ferret','fissure','fizzle','flagon','fletch','flicker','florin',
+    'flurry','fonder','fossil','frenzy','fridge','frolic','fungal','furrow',
+    'gambit','garnet','garret','gibbet','gibbon','gopher','goblet','goblin',
+    'gravel','grotto','grovel','gunnel','harbor','harper','haystack','hermit',
+    'hornet','iguana','jabber','jaguar','jester','juniper','kestrel','lantern',
+    'limpet','lizard','magpie','mallet','marmot','martin','merlin','minnow',
+    'monkey','mortar','muffin','musket','muster','muzzle','nettle','noodle',
+    'nutmeg','obsidian','osprey','parrot','pebble','pellet','pigeon','pillar',
+    'pincer','piranha','pistol','plover','potion','puffin','pulsar','python',
+    'quiver','rabbit','radish','raptor','rascal','ravine','reaper','riddle',
+    'robber','rocket','rodent','rookie','rosary','rudder','runner','saddle',
+    'salmon','sandal','satchel','savage','scythe','seraph','serval','shaman',
+    'shrike','sienna','siren','skimmer','sliver','smudge','sniper','socket',
+    'sorrel','sorrow','spider','splint','sponge','sprout','spurge','squall',
+    'stable','staple','stealth','steppe','sticle','stilts','stoker','stucco',
+    'sundog','sunken','sunder','sunset','symbol','tallow','tamper','tangle',
+    'tartan','tawny','tassel','tattler','thatch','thorax','thrush','tinder',
+    'tinker','tipple','toadstool','toggle','toucan','trowel','tundra','tunnel',
+    'turban','turtle','tussle','tycoon','urchin','victor','vinery','violet',
+    'virago','virtue','visage','walnut','warden','weasel','webbing','wicket',
+    'widget','wigeon','wiggle','willow','winkle','winnow','winter','wisdom',
+    'wombat','wonder','wortle','wyvern',
+];
+
+function randomRoomName() {
+    const pick = () => ROOM_NAME_WORDS[Math.floor(Math.random() * ROOM_NAME_WORDS.length)];
+    let a, b;
+    do { a = pick(); b = pick(); } while (a === b);
+    return a + '-' + b;
+}
+
 async function createRoom() {
-    const roomName = document.getElementById('room-name').value.trim();
-    if (!roomName) return showToast('Enter a room name');
+    const input = document.getElementById('room-name');
+    let roomName = input.value.trim();
+    if (!roomName) {
+        roomName = randomRoomName();
+        input.value = roomName;
+    }
     if (roomName.length > 30) return showToast('Room name too big (>30 chars)');
     const res = await apiPost(API_BASE + 'api/room_create.php', {
         room_name: roomName,
